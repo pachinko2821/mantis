97960c6d5201b02a333dfc15fe5f3309b91a2e1f
Prajyot Chemburkar <pachinko.pachinko2@gmail.com>
2025-02-01 23:54:51 +0530

    updated setup.sh and renamed to init.sh

--- setup/devbox/init.sh
+++ setup/devbox/init.sh
@@ -7,7 +7,7 @@ cd ../../
 pip install -r requirements.txt
 
 # install Corsey
-if [[ ! -d /usr/bin/Corsey ]]; then
+if [ ! -d /usr/bin/Corsey ]; then
     wget https://github.com/s0md3v/Corsy/archive/refs/tags/1.0-rc.zip
     unzip 1.0-rc.zip
     mv Corsy-1.0-rc Corsey
@@ -16,7 +16,11 @@ if [[ ! -d /usr/bin/Corsey ]]; then
 fi
 
 # setup appsmith container
-docker run -d --name appsmith -p 1337:80 -p 1338:443 appsmith/appsmith-ce:v1.59
+if [ ! $(docker ps --format "{{.Names}}"|grep appsmith) ]; then
+    docker run -d --name appsmith -p 1337:80 -p 1338:443 appsmith/appsmith-ce:v1.59
+fi
 
 # setup mongo locally
-docker run -d --name mongo -p 27017:27017 mongodb/mongodb-community-server:7.0.6-ubuntu2204
+if [ ! $(docker ps --format "{{.Names}}"|grep mongo) ]; then
+    docker run -d --name mongo -p 27017:27017 mongodb/mongodb-community-server:7.0.6-ubuntu2204
+fi
